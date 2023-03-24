3eab89c72cb0d647b1a63b689259fa4cc5bf9494
York Jong <yukuan.jiang@gmail.com>
2023-03-24 14:36:15 +0800

    docs: refine docstrings

--- api/op.py
+++ api/op.py
@@ -1,5 +1,6 @@
 """
-Operations (e.g., difference, union, merge) of links between news journal files.
+Operations (e.g., difference, union, merge) of links between markdown lines with
+a sequence of header:links structures.
 """
 __author__ = "York <york.jong@gmail.com>"
 __date__ = "2023/03/23 (initial version) ~ 2023/03/24 (last revision)"
