d8d2d228f8ea7677679117d6284ff4d1803ed4cd
York Jong <yukuan.jiang@gmail.com>
2023-05-04 23:09:14 +0800

    Update subscrip.py

--- api/subscrip.py
+++ api/subscrip.py
@@ -8,11 +8,48 @@ __all__ = [
     'handler',
 ]
 
-import os
 import requests
-import secrets
 from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler
+
+
+import json
+import requests
+from json import JSONDecodeError
+
+
+def token_status(token):
+    '''Check status of a Line Access Token.
+
+    Args:
+        token (str): line access token
+
+    Returns:
+        (dict): a dictionary of the reponsed JSON
+    '''
+    url = 'https://notify-api.line.me/api/status'
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+
+    resp = requests.get(url, headers=headers)
+    try:
+        return resp.json()
+    except JSONDecodeError:
+        print('Response could not be serialized')
+        return {}
+
+
+def token_target(token):
+    '''Get target of a Line Access Token.
+
+    Args:
+        token (str): line access token
+
+    Returns:
+        (str): the target (e.g., the user name or the group name)
+    '''
+    return token_status(token).get('target', '')
 
 
 class handler(BaseHTTPRequestHandler):
@@ -25,6 +62,13 @@ class handler(BaseHTTPRequestHandler):
         params = parse_qs(query)
 
         token = params.get('token', [''])[0]
+        target = token_target(token)
+
+        if not target(token):
+            self.send_response(401)
+            self.end_headers()
+            self.wfile.write(b'Invalid access token')
+            return
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
