47c5dcd1905b8cfd233b7c99e9c8765727cf4cff
York Jong <yukuan.jiang@gmail.com>
2023-05-05 14:46:35 +0800

    Create gdrive.py

---
+++ api/gdrive.py
@@ -0,0 +1,6 @@
+"""
+The module implement operations of files in Google Drive.
+"""
+__author__ = "York <york.jong@gmail.com>"
+__date__ = "2023/05/05 (initial version) ~ 2023/05/05 (last revision)"
+
